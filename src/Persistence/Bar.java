924ab09d684de433e5f034c5653ae688c65e9de7
brunork26 <bruno.r.kieling@hotmail.com>
2018-04-10 20:59:46 -0300

    up

--- src/Persistence/Bar.java
+++ src/Persistence/Bar.java
@@ -6,21 +6,46 @@ public class Bar {
 	private ArrayList <Pessoa> pessoasBar;
 	
 	public Bar() {
-		pessoasBar = new ArrayList<Pessoa>();
-		
+		pessoasBar = new ArrayList<Pessoa>();	
 	}
 	
 	public ArrayList<Pessoa> retornaDados() {
 		return pessoasBar;
 	}
 	
-	public void setPessoa(Pessoa pes) {
+	public void addPessoa(Pessoa pes) {
 		if(pes!=null)
 			pessoasBar.add(pes);
+	}
+	
+	public boolean removePessoa(String cpf) {
+		for(Pessoa p: pessoasBar) {
+			if(p.getCpf().equals(cpf)) {
+				pessoasBar.remove(p);
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public int retornaPessoasBar() {
 		return pessoasBar.size();
 	}
-
+	
+	public int retornaQtdGenero(char genero) {
+		int qtd = 0;
+		for(Pessoa p: pessoasBar) {
+			if(p.getGenero() == genero) {
+				qtd++;
+			}
+		}
+		return qtd;
+	}
+	
+	public Pessoa retornaClienteCpf(String cpf) {
+		for(Pessoa p: pessoasBar) {
+			if(p.getCpf().equals(cpf)) return p;
+		}
+		return null;
+	}
 }
